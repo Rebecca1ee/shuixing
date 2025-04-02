dfb8c8974bc174c4c62f97bac2cf4604ab59f648
Rebecca1ee <rebecca_lee_@outlook.com>
2025-04-02 00:33:28 -0500

    Add files via upload

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: '水星逆行查询',
+  description: '查询当日水星是否逆行',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="zh">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
